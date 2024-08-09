a738fc1e8dca07de3aa61f65afc18e55f6ced1ae
ehdtn0321 <ehdtn0321@naver.com>
2024-08-09 23:40:53 +0900

    🔨change: Small Chip changed
    css style 수정
    - active state 추가
    - !important 제거
    - "profileBackground" styled component 추가
    onClick prop 추가
    - callback함수 적용
    primary prop 추가
    - primary prop에 따른 style 수정

--- src/components/Chip/Small.tsx
+++ src/components/Chip/Small.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components"
 
-const ChipSmallWrapper = styled.div`
+const ChipSmallWrapper = styled.div<{ primary: boolean }>`
   display: flex;
   flex-direction: row;
   gap: 6px;
@@ -8,34 +8,42 @@ const ChipSmallWrapper = styled.div`
   align-items: center;
   border-radius: 6px;
   padding: 6px 8px;
-  background: #4e5053;
+  background: #242424;
   height: 40px;
   width: fit-content;
 
+  ${({ primary }) => primary && `color: #47F998;`};
+
   &:hover {
-    cursor: pointer !important;
-    background-color: rgba(255, 255, 255, 0.1) !important;
+    cursor: pointer;
+    background: rgba(255, 255, 255, 0.1);
+  }
+
+  &:active {
+    background: rgba(255, 255, 255, 0.2);
   }
 `
 
+const ProfileBgWrapper = styled.div<{ iconBgColor: string }>`
+  background: ${({ iconBgColor }) => (iconBgColor ? iconBgColor : "##252627")};
+  left: 8px;
+  border-radius: 30px;
+  height: 30px;
+  width: 30px;
+`
+
 interface IChipProps {
-  item: { icon: string; label: string; value: string }
+  item: { icon: string; label: string; value: string; iconBgColor?: string }
+  onClick: (value: string) => void
+  primary?: boolean
 }
 
-const ChipSmall = ({ item }: IChipProps) => {
+const ChipSmall = ({ item, onClick, primary }: IChipProps) => {
   return (
-    <ChipSmallWrapper>
-      <div
-        style={{
-          background: "rgba(37, 38, 39, 1)",
-          left: "8px",
-          borderRadius: "30px",
-          height: "30px",
-          width: "30px",
-        }}
-      >
+    <ChipSmallWrapper primary={primary} onClick={() => onClick(item.value)}>
+      <ProfileBgWrapper iconBgColor={item.iconBgColor}>
         <img src={item.icon} height={30} style={{ zIndex: 1 }} />
-      </div>
+      </ProfileBgWrapper>
       {item.label}
     </ChipSmallWrapper>
   )
