bf498ba0c97284a4631239a99acd09a1a644f512
ehdtn0321 <ehdtn0321@naver.com>
2024-08-09 23:39:23 +0900

    🔨change: Change file name & Change Code
    css style 수정
    - active state 추가
    - !important 제거
    onClick prop 추가
    - callback함수 적용
    primary prop 추가
    - primary prop에 따른 style 수정

--- src/components/Chip/Medium.tsx
+++ src/components/Chip/Medium.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components"
 
-const ChipMiddleWrapper = styled.div`
+const ChipMediumWrapper = styled.div<{ primary: boolean }>`
   display: flex;
   flex-direction: row;
   gap: 6px;
@@ -8,29 +8,37 @@ const ChipMiddleWrapper = styled.div`
   align-items: center;
   border-radius: 6px;
   padding: 8px 12px;
-  background: #4e5053;
+  background: #242424;
   height: 40px;
   width: fit-content;
 
   font-size: 18px;
   line-height: 25px;
 
+  ${({ primary }) => primary && `color: #47F998;`};
+
   &:hover {
-    cursor: pointer !important;
-    background-color: rgba(255, 255, 255, 0.1) !important;
+    cursor: pointer;
+    background: rgba(255, 255, 255, 0.1);
+  }
+
+  &:active {
+    background: rgba(255, 255, 255, 0.2);
   }
 `
 
 interface IChipProps {
   item: { icon: string; label: string; value: string }
+  onClick: (value: string) => void
+  primary?: boolean
 }
 
-const ChipMiddle = ({ item }: IChipProps) => {
+const ChipMiddle = ({ item, onClick, primary }: IChipProps) => {
   return (
-    <ChipMiddleWrapper>
+    <ChipMediumWrapper primary={primary} onClick={() => onClick(item.value)}>
       <img src={item.icon} height={20} style={{ zIndex: 1 }} />
       {item.label}
-    </ChipMiddleWrapper>
+    </ChipMediumWrapper>
   )
 }
 
